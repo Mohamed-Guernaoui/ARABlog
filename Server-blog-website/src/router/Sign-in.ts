a29e143092d92e4df549ca52ba6c5250969cfe88
Mohamed <looklike882@gmail.com>
2024-11-25 13:10:07 +0100

    [edit]

--- Server-blog-website/src/router/Sign-in.ts
+++ Server-blog-website/src/router/Sign-in.ts
@@ -1,20 +1,20 @@
-import { DeleteSessoinHandeler, createSessionHandler, getUserSessionHandeler } from '../controller/session.controller'
-import Validate from '../Middlewares/validateRessources'
-import { CreateUserZodShema } from '../Schema/user.schema'
-import { CreateUserHandler } from '../controller/user.controller'
-import { CreatesessionSchema } from '../Schema/session.Schema'
 import { Router } from 'express'
-import requireUser from '../Middlewares/requireUser'
+import { DeleteSessionHandeler, createSessionHandler, getUserSessionHandeler } from '../controller/session.controller'
+import { CreateUserZodShema } from '../Schema/user.schema'
+import { createUserHandler } from '../controller/user.controller'
+import { CreatesessionSchema } from '../Schema/session.Schema'
 import { FindPostHandler } from '../controller/Post.Controller'
 import { GetPostSchema } from '../Schema/Post.schema'
 import { CreateblogSchema } from '../Schema/Blog.schema'
 import { createBlogHandler } from '../controller/Blog.controller'
-// DEBUG: all requests should hace requieUser Middelware
+import Validate from '../Middlewares/validateRessources'
+import requireUser from '../Middlewares/requireUser'
+// NOTE: all requests should hace requieUser Middelware
 export default (router: Router) => {
-  router.post('/api/sign-up', Validate(CreateUserZodShema), CreateUserHandler)
+  router.post('/api/sign-up', Validate(CreateUserZodShema), createUserHandler)
   router.post('/api/sign-in', Validate(CreatesessionSchema), createSessionHandler)
   router.get('/api/session', requireUser, getUserSessionHandeler)
-  router.delete('/api/session', requireUser, DeleteSessoinHandeler)
+  router.delete('/api/session', requireUser, DeleteSessionHandeler)
   router.get('/api/posts/:postid', Validate(GetPostSchema), FindPostHandler)
   router.post('/api/newBlog', [requireUser, Validate(CreateblogSchema)], createBlogHandler)
 }
