e6558f83f3d6915224e70fe4a643d9e6c2cf91e1
Mohamed <looklike882@gmail.com>
2024-11-25 13:19:10 +0100

    [fix]: JWT utils

--- Server-blog-website/src/utils/jwt.utils.ts
+++ Server-blog-website/src/utils/jwt.utils.ts
@@ -1,23 +1,32 @@
 import jwt from 'jsonwebtoken'
+// import * as crypto from "crypto";
 import config from 'config'
+const publicKey = config.get<string>('publicKey')
+const privateKey = config.get<string>('privateKey')
 
-const PublicKey = config.get<string>('publicKey')
-const PrivateKey = config.get<string>('privateKey')
-
+/**
+ * Signs a given object with a JSON Web Token, using the RS256 algorithm
+ * @param object The object to be signed
+ * @param options Optional options for the signing
+ * @returns A signed token
+ */
 export function SignJwt(object: Object, options?: jwt.SignOptions | undefined) {
-  return jwt.sign(object, PrivateKey, {
+  return jwt.sign(object, privateKey, {
     ...(options && options),
     algorithm: 'RS256',
   })
 }
+// Verify the token with the same algorithm
 export function verifyJwt(token: string) {
   try {
-    const decoded = jwt.verify(token, PublicKey)
+    console.log(`🚀 ~ file: jwt.utils.ts:32 ~ verifyJwt ~ verificating`)
+    const decoded = jwt.verify(token, publicKey, { algorithms: ['RS256'] })
     return {
       valid: true,
       expired: false,
       decoded,
     }
+
   } catch (e) {
     return {
       valid: false,
