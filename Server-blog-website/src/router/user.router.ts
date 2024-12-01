e5b615342e0beb339b74e31b2630e240707cefae
Mohamed <looklike882@gmail.com>
2024-12-01 22:58:08 +0100

    [add] : new route

--- Server-blog-website/src/router/user.router.ts
+++ Server-blog-website/src/router/user.router.ts
@@ -1,6 +1,7 @@
-import { getAuthorsBysearch } from '../controller/user.controller'
+import { getAuthorsBysearch ,getAuthorByIdController} from '../controller/user.controller'
 import { Router } from 'express'
 
 export default (router: Router) => {
-  router.post('/api/search-authors', getAuthorsBysearch)
+  router.post('/api/search-authors', getAuthorsBysearch);
+  router.post('/api/author',getAuthorByIdController);
 }
